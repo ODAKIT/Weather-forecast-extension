b400735475269cb24e578623975703dd64cfb2d9
ODAKIT <tanlalan123@gmail.com>
2020-03-25 16:18:46 +0700

    update map id call from api , change icon by id

--- MyApp/Weather forecasts chrome extension/js/getweather.js
+++ MyApp/Weather forecasts chrome extension/js/getweather.js
@@ -1,8 +1,96 @@
 // API KEY : d9831ec7ba29dfb58680b33f06b00b7b
 
+const idMap = new Map([
+    [200, "thunter"],
+    [201, "thunter"],
+    [202, "thunter"],
+    [210, "thunter"],
+    [211, "thunter"],
+    [212, "thunter"],
+    [221, "thunter"],
+    [230, "thunter"],
+    [231, "thunter"],
+    [232, "thunter"],
 
+    [300, "small_rain"],
+    [301, "small_rain"],
+    [302, "small_rain"],
+    [310, "small_rain"],
+    [311, "small_rain"],
+    [312, "small_rain"],
+    [313, "small_rain"],
+    [314, "small_rain"],
+    [321, "small_rain"],
+    
+    [500, "rain"],
+    [501, "rain"],
+    [502, "rain"],
+    [503, "rain"],
+    [504, "rain"],
+    [511, "freezing"],
+    [520, "small_rain"],
+    [521, "small_rain"],
+    [522, "small_rain"],
+    [531, "small_rain"],
+
+
+    [500, "rain"],
+    [501, "rain"],
+    [502, "rain"],
+    [503, "rain"],
+    [504, "rain"],
+    [511, "freezing"],
+    [520, "small_rain"],
+    [521, "small_rain"],
+    [522, "small_rain"],
+    [531, "small_rain"],
+
+
+    [600, "freezing"],
+    [601, "freezing"],
+    [602, "freezing"],
+    [611, "freezing"],
+    [612, "freezing"],
+    [613, "freezing"],
+    [615, "freezing"],
+    [616, "freezing"],
+    [620, "freezing"],
+    [621, "freezing"],
+    [622  ,"freezing"],
+
+
+
+    [701, "mist"],
+    [711, "mist"],
+    [721, "mist"],
+    [731, "mist"],
+    [741, "mist"],
+    [751, "mist"],
+    [761, "mist"],
+    [762, "mist"],
+    [771, "mist"],
+    [781, "mist"],
+
+    [800, "sun12"],
+    [801, "sun12"],
+    [802, "sun12"],
+    [803, "sun12"],
+    [804, "sun12"],
+    
+
+
+
+
+
+
+    ['default', ["sunny", '']]
+])
 var weather = {};
 
+//get day
+var today = new Date();
+var hours = today.getHours();
+var time = hours + ":" + today.getMinutes();
 
 $.ajax({
     type: "GET",
@@ -19,6 +107,7 @@ $.ajax({
         weather.city = data.name;
         weather.weather_id = data.weather[0].id; // id weather de lay trang thai;
         weather.weather_enum = data.weather[0].description;
+        ActiveIcon(data.weather[0].id);
         // checkWeather(weather.weather_id); //hàm này sẽ được giải thích ở dưới, nó xử  lý hiện tượng thời tiết
         weather.temp = data.main.temp;
         weather.maxTemp = data.main.temp_max;
@@ -30,9 +119,7 @@ $.ajax({
         $(".location .place").html(weather.city);
         $(".date .dayOfWeek").html(weather.dayOfWeek);
         $(".date .dateTime").html(weather.date);
-        $(".date .time").html(weather.curTime);
-
-        console.log(weather.curTime);
+        $(".date .time").html(time);
 
 
 
@@ -44,4 +131,39 @@ $.ajax({
 
     }
 });
+
+function icon(icon_name) {
+    $(".weather .icon").html(icon_name);
+}
+
+function ActiveIcon(id){
+    let action = idMap.get(id);
+    $(".wico").attr("src","../imgsrc/"+action+".svg");
+}
+ 
+
+//change color and icon 
+
+
+var bgColor = "rgb(255, 255, 255)";
+
+if(hours>5 && hours<=7){
+    bgColor = "#ffbc05";    
+}
+else if(hours<=12){
+    bgColor = "rgb(255, 188, 5)";    
+}
+else if(hours <=18){
+    bgColor = "rgb(255, 165, 0)";
+
+}
+else{
+    bgColor = "rgb(230, 221, 228)";
+}
+
+$(".weather .ico").css("background-color", bgColor);
+
+
+
+
 
